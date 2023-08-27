ab9c050ecbb03063520baed22d17bae44d17b605
Sam Storment <sjstorment22@gmail.com>
2023-08-27 02:41:31 -0500

    Initial Commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,12 @@
+
+<div>
+    <a href="https://github.com/sveltejs/kit">External Link</a>
+    <br>
+    <br>
+    <a href="/other">Internal Link</a>
+</div>
+
+
+{#each { length: 100 } as _}
+    <div style="padding: 1rem;">take up space to cause scroll</div>
+{/each}
---
+++ src/routes/other/+page.svelte
@@ -0,0 +1,7 @@
+
+<h1 style="background-color: red;">Visually distinct element to showcase flicker</h1>
+
+
+{#each { length: 100 } as _}
+    <div style="padding: 1rem;">take up space to cause scroll</div>
+{/each}
