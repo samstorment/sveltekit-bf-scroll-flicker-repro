02d6a6e1a15dd065e2f0fb740dc01f76df50319f
samstorment <sjstorment22@gmail.com>
2023-09-12 19:38:33 -0500

    Updated sveltekit version to use onNavigate and added +layout.svelte

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,8 @@
+<script>
+    import { onNavigate } from "$app/navigation";
+
+    // uncomment to delay navigation - shows flicker much more clearly
+    // onNavigate(() => new Promise(res =>  setTimeout(res, 500)));
+</script>
+
+<slot />
