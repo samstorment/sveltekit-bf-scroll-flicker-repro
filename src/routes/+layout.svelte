7aa6114be8ba5631ee896f52dcd298763902e5d5
Sam Storment <sjstorment22@gmail.com>
2024-02-15 16:57:06 -0600

    Update packages

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,7 +2,7 @@
     import { onNavigate } from "$app/navigation";
 
     // uncomment to delay navigation - shows flicker much more clearly
-    // onNavigate(() => new Promise(res =>  setTimeout(res, 500)));
+    // onNavigate(() => new Promise(res => setTimeout(res, 500)));
 </script>
 
 <slot />
